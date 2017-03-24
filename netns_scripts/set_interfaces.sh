d6b3663b5a130232187b490ab13f7dc7bdc1ffd8
Margus Ernits <marguss.ernits@rangeforce.com>
2017-03-24 10:38:56 -0500

    set interfaces according to topology

---
+++ netns_scripts/set_interfaces.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./create_namespace.sh eth2 eth2_ns 192.168.99.1/24 192.168.99.254
+./create_namespace.sh eth3 eth3_ns 192.168.88.1/24 192.168.88.254
+./create_namespace.sh eth4 eth4_ns 192.168.109.1/24 192.168.109.254
