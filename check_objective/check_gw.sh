dac3478f08586a959f93b9c9ce541a3ffe15c9e4
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-03-24 15:28:11 -0500

    gw check script

--- check_objective/check_gw.sh
+++ check_objective/check_gw.sh
@@ -2,6 +2,10 @@
 # Author: Olari Pipenberg
 # NOTE that gw and interfaces check script cant be run same time
 
+# Codes 
+# exit 0 - gw is configured
+# exit 1 - gw is not configured 
+
 GW_ADDR="192.168.99.254"
 NSPACE="eth2_ns"
 NSPACE_DEV="eth2"
@@ -13,7 +17,7 @@ DST_IP="30.0.127.1"
 /sbin/ip netns exec $NSPACE ip route add default via $GW_ADDR dev $NSPACE_DEV
 
 # Start capturing icmp traffic
-tshark -i $CAP_DEV -f "icmp" > /tmp/traffic < /dev/null 2>&1 &
+tshark -i $CAP_DEV -f "icmp && host $DST_IP && host $SRC_IP" > /tmp/traffic < /dev/null 2>&1 &
 
 # Send icmp traffic
 ip netns exec $NSPACE ping -c 1 -w 4 $DST_IP > /dev/null
@@ -24,8 +28,15 @@ pkill tshark
 # Wait for tshark buffer
 sleep 1
 
-cat /tmp/traffic
-#rm /tmp/traffic
-
 # Delete gateway
 /sbin/ip netns exec $NSPACE ip route del default via $GW_ADDR dev eth2
+
+grep "$SRC_IP -> $DST_IP" /tmp/traffic > /dev/null || 
+				          {
+					   echo "gw is not configured"
+					   exit 1
+					  }
+echo "gw is configured"
+exit 0 # gw is configured 
+rm /tmp/traffic
+
