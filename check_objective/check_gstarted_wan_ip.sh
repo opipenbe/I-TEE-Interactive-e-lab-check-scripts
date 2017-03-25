9ae63374eb6b62ea8e572c76204eb3387dd17e99
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-03-25 15:21:48 -0500

    subnet check script

--- check_objective/check_gstarted_wan_ip.sh
+++ check_objective/check_gstarted_wan_ip.sh
@@ -1,5 +1,5 @@
 ping -W 2 -c 1 30.0.127.2 > /dev/null &&
 echo "WAN interface is configured" || { echo "WAN interface is not configured"
-                                      exit 1
+                                      	exit 1
                                     }
 exit 0
--- netns_scripts/set_interfaces.sh
+++ netns_scripts/set_interfaces.sh
@@ -4,3 +4,7 @@ cd "$(dirname "$0")"
 ./create_namespace.sh eth2 eth2_ns 192.168.99.1/24
 ./create_namespace.sh eth3 eth3_ns 192.168.88.1/24
 ./create_namespace.sh eth4 eth4_ns 192.168.109.1/24
+
+# this interface is for subnet checking
+./create_namespace.sh eth5 eth5_ns 30.0.127.30/27
+
