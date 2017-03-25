4b18d25e58d261bc2a43707def3ec1060281e506
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-03-25 09:04:59 -0500

    fix

--- check_objective/check_vgstarted_all_interfaces.sh
+++ check_objective/check_vgstarted_all_interfaces.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 ping -W 2 -c 1 30.0.127.2 > /dev/null &&
+ping -W 2 -c 1 30.0.127.3 > /dev/null &&
+ping -W 2 -c 1 30.0.127.4 > /dev/null &&
+ping -W 2 -c 1 30.0.127.5 > /dev/null &&
+ping -W 2 -c 1 30.0.127.6 > /dev/null &&
 ./check_ip.sh eth2_ns 192.168.99.254 &&
 ./check_ip.sh eth3_ns 192.168.88.254 &&
 ./check_ip.sh eth4_ns 192.168.109.254 &&
