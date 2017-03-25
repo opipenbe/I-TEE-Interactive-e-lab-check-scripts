552191a9c92b4727970f2d41bee9679032e057f1
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-03-25 07:02:53 -0500

    debug output

--- check_objective/check_gstarted_wan_ip.sh
+++ check_objective/check_gstarted_wan_ip.sh
@@ -1,3 +1,5 @@
 ping -W 2 -c 1 30.0.127.2 > /dev/null &&
-echo "works" &&
+echo "Interfaces are configured" || { echo "Interfaces are not configured"
+                                      exit 1
+                                    }
 exit 0
