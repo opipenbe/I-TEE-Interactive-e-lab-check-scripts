ecad1513f42bdbcff42ba206c4d7351472d7fd0a
opipenbe <olari.pipenberg@gmail.com>
2017-03-26 20:23:35 +0300

    Update check_gstarted_wan_ip.sh

--- check_objective/check_gstarted_wan_ip.sh
+++ check_objective/check_gstarted_wan_ip.sh
@@ -1,5 +1,5 @@
 cd "$(dirname "$0")"
-ping -W 2 -c 1 30.0.127.2 > /dev/null &&
+ping -W 1 -c 1 30.0.127.2 > /dev/null &&
 bash check_lower_subnet_prefix.sh 30.0.127.2 eth5_ns eth5 30.0.127.30/27 &&
 bash check_higher_subnet_prefix.sh 30.0.127.2 eth5_ns eth5 30.0.127.14/28 &&
 echo "WAN interface is configured" || { echo "WAN interface is not configured"
