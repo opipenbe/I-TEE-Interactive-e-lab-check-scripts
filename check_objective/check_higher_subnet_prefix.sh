7570df2259a3f0a53e5d37da0d9e960166651bba
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-03-26 03:57:19 -0500

    prefix fix

--- check_objective/check_higher_subnet_prefix.sh
+++ check_objective/check_higher_subnet_prefix.sh
@@ -7,23 +7,29 @@ NSPACE=$2
 DEV=$3
 HIGH_IP=$4
 
-# bash check_lower_subnet_prefix.sh 30.0.127.2 eth5_ns eth5 30.0.127.14/28
+# bash check_higher_subnet_prefix.sh 30.0.127.2 eth5_ns eth5 30.0.127.14/28
 # TODO check if namespace is correct.
 
 
 CURRENT_IP=$(ip netns exec $NSPACE ip -o -4 addr list $DEV | awk '{print $4}')
+
+# Check current ip, if address empty, then exit
+if [ -z "$CURRENT_IP" ]; then
+	exit 2
+fi
+
 ip netns exec $NSPACE ip addr flush dev $DEV
 ip netns exec $NSPACE ip addr add $HIGH_IP dev $DEV
 
 ip netns exec $NSPACE ping -w 1 -c 1 $IP_ADR > /dev/null
 
 if [ $? -eq 0 ]; then
-        echo "subnet ok"
+        echo "no higher prefix"
 	ip netns exec $NSPACE ip addr flush dev $DEV
 	ip netns exec $NSPACE ip addr add $CURRENT_IP dev $DEV	
         exit 0
 else
-	echo "wrong subnet"
+	echo "higher prefix"
 	ip netns exec $NSPACE ip addr flush dev $DEV
         ip netns exec $NSPACE ip addr add $CURRENT_IP dev $DEV
 	exit 1
--- check_objective/check_lower_subnet_prefix.sh
+++ check_objective/check_lower_subnet_prefix.sh
@@ -10,20 +10,25 @@ LOW_IP=$4
 # bash check_lower_subnet_prefix.sh 30.0.127.2 eth5_ns eth5 30.0.127.30/27
 # TODO check if namespace is correct.
 
-
 CURRENT_IP=$(ip netns exec $NSPACE ip -o -4 addr list $DEV | awk '{print $4}')
+
+# Check current ip, if address empty then exit
+if [ -z "$CURRENT_IP" ]; then
+        exit 2
+fi
+
 ip netns exec $NSPACE ip addr flush dev $DEV
 ip netns exec $NSPACE ip addr add $LOW_IP dev $DEV
 
 ip netns exec $NSPACE ping -w 1 -c 1 $IP_ADR > /dev/null
 
 if [ $? -ne 0 ]; then
-        echo "subnet ok"
+        echo "no lower prefix"
 	ip netns exec $NSPACE ip addr flush dev $DEV
 	ip netns exec $NSPACE ip addr add $CURRENT_IP dev $DEV	
         exit 0
 else
-	echo "wrong subnet"
+	echo "lower prefix"
 	ip netns exec $NSPACE ip addr flush dev $DEV
         ip netns exec $NSPACE ip addr add $CURRENT_IP dev $DEV
 	exit 1
