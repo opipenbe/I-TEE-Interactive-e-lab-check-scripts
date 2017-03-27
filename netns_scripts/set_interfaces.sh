1620780aa59bfb7fa60ab84b93b4bc79e4c4af48
opipenbe <olari.pipenberg@gmail.com>
2017-03-27 10:11:05 +0300

    Update set_interfaces.sh

--- netns_scripts/set_interfaces.sh
+++ netns_scripts/set_interfaces.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Get working directory
 cd "$(dirname "$0")"
-# this interface is for workstation subnet checking
+# this namespace is for workstation subnet checking
 ./create_namespace.sh eth2 eth2_ns 192.168.99.1/24
-# this interface is for server subnet checking
+# this namespace is for server subnet checking
 ./create_namespace.sh eth3 eth3_ns 192.168.88.1/24
-# this interface is for guest subnet checking
+# this namespace is for guest subnet checking
 ./create_namespace.sh eth4 eth4_ns 192.168.109.1/24
-# this interface is for ISP subnet checking
+# this namespace is for ISP subnet checking
 ./create_namespace.sh eth5 eth5_ns 30.0.127.30/27
 
