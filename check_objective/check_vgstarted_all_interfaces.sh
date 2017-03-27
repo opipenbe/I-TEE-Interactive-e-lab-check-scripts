5e7e7913ee07c263943537d0829e78d90a223285
opipenbe <olari.pipenberg@gmail.com>
2017-03-27 10:15:46 +0300

    Update check_vgstarted_all_interfaces.sh

--- check_objective/check_vgstarted_all_interfaces.sh
+++ check_objective/check_vgstarted_all_interfaces.sh
@@ -25,7 +25,7 @@ ping -W 1 -c 1 30.0.127.7 > /dev/null &&
 bash check_lower_subnet_prefix.sh 30.0.127.7 eth5_ns eth5 30.0.127.30/27 &&
 bash check_higher_subnet_prefix.sh 30.0.127.7 eth5_ns eth5 30.0.127.14/28 &&
 
-# There is no need to check higher prefix in internal networks, becouse this machine internal network addresses end with  x.x.x.1,
+# There is no need to check higher prefix in internal networks, becouse checker machine (ISP) internal network addresses end with  x.x.x.1,
 # addresses to be checked are end with x.x.x.254 and we are dealing with /24 prefix.
 ./check_ip.sh eth2_ns 192.168.99.254 &&
 bash check_lower_subnet_prefix.sh 192.168.99.254 eth2_ns eth2 192.168.98.1/23 &&
