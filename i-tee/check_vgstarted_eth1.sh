fbf3b1a36e78fdde0b56eb0e9483172cee9b570b
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-03-29 22:07:33 +0300

    check

--- i-tee/check_vgstarted_eth1.sh
+++ i-tee/check_vgstarted_eth1.sh
@@ -49,7 +49,7 @@ VYOSGSTARTED () {
 
 	if [ $? -eq 0 ]; then
 		# Run objectiveschecks.py and update VirtualTa with correct value
-		echo -e "\nWan interface configured! Date: `date`\n" && touch $CheckFile
+		echo -e "\neth1 interface configured! Date: `date`\n" && touch $CheckFile
         	$DIR/objectivechecks.py $Uname True || echo -e "\nFailed to run $DIR/objectiveschecks.py! Date: `date`" >&2 && exit 1
         	exit 0			
 	fi
