46ffaef4cc4d9f65a245229397e5db43551d619c
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-03-30 17:20:37 +0300

    second lab prepare script

--- i-tee/lab_init.sh
+++ i-tee/lab_init.sh
@@ -5,9 +5,9 @@ export LAB_USERNAME=$(dmidecode -s bios-release-date)
 dmidecode -s bios-version |grep 'vyos' >/dev/null 2>&1
 if [ $? -eq 0 ]
 then
-    LAB_ID=P4ojLSRryaRReCYdM
+   LAB_ID=P4ojLSRryaRReCYdM
 else
-    LAB_ID=P4ojLSRryaRReCYdM
+   LAB_ID=P4ojLSRryaRReCYdM
 fi
 
 cat > lab.ini <<EOC
@@ -16,4 +16,6 @@ ta_key = 06473dab8ae4c8486b70fe59782d54e2
 virtualta_hostname = https://i-tee.itcollege.ee:8433
 lab_id = $LAB_ID
 EOC
+
+export LAB_ID
 
