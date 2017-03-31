67836c277b6e01c289a15da87cbb32964586ac65
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-03-31 19:34:12 +0300

    ssh password auth check + port

--- check_objective/check_ssh.sh
+++ check_objective/check_ssh.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 KEY_PATH="/root/e-lab-check-scripts/check_objective/ssh/id_rsa"
-ssh -q -i $KEY_PATH vyos@30.0.127.2 exit
-#echo $?
+USERNAME="vyos"
+IP="30.0.127.2"
+PORT="22"
+
+# Checks password auth
+ssh -o PreferredAuthentications=none -p $PORT $USERNAME@$IP 2>&1 >/dev/null | grep password > /dev/null
+if [ $? -eq 0  ]; then
+  echo "ssh password-auth is enabled (error)"
+  exit 1
+fi
+
+# Checks public-key auth
+ssh -q -i $KEY_PATH -p $PORT $USERNAME@$IP 'exit'
 if [ $? -eq 0  ]; then
   echo "ssh-public-key-auth is configured"
   exit 0
 else
   echo "ssh-public-key-auth is not configured"
-  exit 1
+  exit 2
 fi
 
 
