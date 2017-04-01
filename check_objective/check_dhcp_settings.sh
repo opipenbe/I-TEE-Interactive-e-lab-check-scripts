2c205719e65983136a00bc735f2798ed772bb4aa
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-01 19:48:22 +0300

    dhcp pool check functionality (very experimenatal)

--- check_objective/check_dhcp_settings.sh
+++ check_objective/check_dhcp_settings.sh
@@ -9,15 +9,43 @@
 #   exit 4 - wrong dns server
 #   exit 5 - wrong lease
 
-DEV=eth2
-
+START_IP="192.168.99.15/24"
+END_IP="192.168.99.160/24"
 NETWORK="192.168.99.0"
 SUBNET_MASK="255.255.255.0"
 GW="192.168.99.254"
 DNS_SERVER="192.168.99.254"
 LEASE="86400"
+NSPACE="eth2_ns"
+DEV="eth2"
 
-INPUT=$(ip netns exec eth2_ns dhcpcd -T $DEV 2> /dev/null)
+# Saving current ip
+CURRENT_IP=$(ip netns exec $NSPACE ip -o -4 addr list $DEV | awk '{print $4}')
+# Check current ip, if address empty then exit
+if [ -z "$CURRENT_IP" ]; then
+        exit 2
+fi
+
+# New ip
+ip netns exec $NSPACE ip addr flush dev $DEV
+ip netns exec $NSPACE ip addr add $START_IP dev $DEV
+ip netns exec $NSPACE dhcping -c $START_IP -s 192.168.99.254 || {
+								  echo "wrong dhcp pool start address"
+								  exit 1
+								}
+ip netns exec $NSPACE ip addr flush dev $DEV
+ip netns exec $NSPACE ip addr add $END_IP dev $DEV
+ip netns exec $NSPACE dhcping -c $END_IP -s 192.168.99.254 || {
+								echo "wrong dhcp pool end address"
+								exit 1
+							      }
+
+# Restoring ip
+ip netns exec $NSPACE ip addr flush dev $DEV
+ip netns exec $NSPACE ip addr add $CURRENT_IP dev $DEV
+
+
+INPUT=$(ip netns exec $NSPACE dhcpcd -T $DEV 2> /dev/null)
 
 # Check network
 echo $INPUT | grep "new_network_number='$NETWORK'" > /dev/null
