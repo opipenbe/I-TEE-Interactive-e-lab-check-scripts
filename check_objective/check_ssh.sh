590bddf6dd981b200e2f952df822fd2e60bc7890
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-02 15:06:10 +0300

    disable ssh fingerprint

--- check_objective/check_ssh.sh
+++ check_objective/check_ssh.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 KEY_PATH="/root/e-lab-check-scripts/check_objective/ssh/id_rsa"
-USERNAME="vyos"
+USERNAME="zeus"
 IP="30.0.127.2"
-PORT="22"
+PORT="28960"
 
 # Checks password auth
-ssh -o PreferredAuthentications=none -p $PORT $USERNAME@$IP 2>&1 >/dev/null | grep password > /dev/null
+ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=none -p $PORT $USERNAME@$IP 2>&1 >/dev/null | grep password > /dev/null
 if [ $? -eq 0  ]; then
   echo "ssh password-auth is enabled (error)"
   exit 1
 fi
 
 # Checks public-key auth
-ssh -q -i $KEY_PATH -p $PORT $USERNAME@$IP 'exit'
+ssh -o StrictHostKeyChecking=no -q -i $KEY_PATH -p $PORT $USERNAME@$IP 'exit'
 if [ $? -eq 0  ]; then
   echo "ssh-public-key-auth is configured"
   exit 0
