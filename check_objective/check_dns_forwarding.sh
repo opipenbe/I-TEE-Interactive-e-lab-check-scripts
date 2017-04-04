c924084746e83ec5988943cf80b5fb556c54e6cb
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-04 11:13:33 +0300

    dns static map check script

--- check_objective/check_dns_forwarding.sh
+++ check_objective/check_dns_forwarding.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 NSPACE=$1
-HOSTNAME="vyos"
 IP="$2"
+HOSTNAME="$3"
 
-if [ $# -ne 2 ]; then
-        echo "Usage: $(basename $0) <namespace> <ipv4 address>"
+if [ $# -ne 3 ]; then
+        echo "Usage: $(basename $0) <namespace> <ipv4 address> <hostname>"
         exit 1
 fi
 
 ip netns exec $NSPACE host -W 1 $HOSTNAME $IP > /dev/null || {
-  							  echo "dns forwarding not configured"
+  							  echo "dns forwarding for $HOSTNAME  is not configured in $IP"
   							  exit 1
 							}
-echo "dns forwarding is configured"
+echo "dns forwarding for $HOSTNAME is configured in $IP"
 exit 0
