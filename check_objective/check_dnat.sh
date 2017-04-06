87526d901e878a0b58146484161b3d1de909699a
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-06 15:50:15 +0300

    check dnat

---
+++ check_objective/check_dnat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+PORT="80"
+ADDRESS="30.0.127.3"
+
+nc -z $ADDRESS $PORT
+if [ $? -ne 0  ]; then
+  "PAT for address: $ADDRESS and port: $PORT is not configured"
+  exit 1
+fi
+
+# Check if dnat rule is too wide (ssh should not be open)
+nc -z $ADDRESS 22
+if [ $? -ne 0  ]; then
+  echo "PAT for address: $ADDRESS and port: $PORT is configured"
+  exit 0
+fi
+
+echo "DNAT rule is too wide"
+exit 1
+
