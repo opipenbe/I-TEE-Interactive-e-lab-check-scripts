3025c355fd9e87080509c77487d298cc2cc02132
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-06 09:51:08 +0300

    snat fix

---
+++ check_objective/check_snat_workstation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NSPACE="eth2_ns"
+DEV="eth2"
+SNAT_IP="30.0.127.4"
+WEBSERVER_ADDR="30.0.127.1/api.php"
+GW="192.168.99.254"
+
+# add default gw
+ip netns exec $NSPACE ip route add default via $GW
+
+EXT_IP=$(ip netns exec $NSPACE curl -s --max-time 1  $WEBSERVER_ADDR | jq -r '.ip')
+if [[ $EXT_IP == $SNAT_IP ]]; then
+	echo "snat for $SNAT_IP is configured"
+	# delete default gw
+	ip netns exec $NSPACE ip route del default via $GW
+	exit 0
+fi
+
+echo "snat for $SNAT_IP is not configured"
+
+# delete default gw
+ip netns exec $NSPACE ip route del default via $GW
+
+exit 1
