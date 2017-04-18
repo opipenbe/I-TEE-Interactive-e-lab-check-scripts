7e29c3fcf1be34ad4e571d1c2e1c64b69df52f9e
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-18 15:24:12 +0300

    zone creation scripts performance + nping

--- config/packages.sh
+++ config/packages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # All required packages for check scripts
-apt-get install -y netcat curl jq dnsmasq dhcpcd5 dhcping tshark apache2 php5 libapache2-mod-php5 php5-mcrypt
+apt-get install -y fping netcat curl jq dnsmasq dhcpcd5 dhcping tshark apache2 php5 libapache2-mod-php5 php5-mcrypt
