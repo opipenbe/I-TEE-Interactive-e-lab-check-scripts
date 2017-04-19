31d3f6e2a2823bb1ddc5d469aa4db792402666a0
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-19 14:57:26 +0300

    prep

--- check_objective/check_fw_dns.sh
+++ check_objective/check_fw_dns.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-bash check_dns_forwarding.sh eth2_ns 192.168.99.254 gw.lab.zz
-bash check_dns_forwarding.sh eth3_ns 192.168.88.254 gw.lab.zz 
+bash check_dns_forwarding.sh eth2_ns 192.168.99.254 gw.lab.zz > /dev/null &&
+bash check_dns_forwarding.sh eth3_ns 192.168.88.254 gw.lab.zz > /dev/null &&
+! bash check_dns_forwarding.sh eth4_ns 192.168.109.254 gw.lab.zz > /dev/null &&
+! bash check_dns_forwarding.sh eth5_ns 30.0.127.2 gw.lab.zz > /dev/null &&
+echo "DNS allowed (ok)" &&
+exit 0
+
+echo "DNS is not allowed (error)"
+exit 1
--- check_objective/check_fw_icmp_intra.sh
+++ check_objective/check_fw_icmp_intra.sh
@@ -1,20 +1,18 @@
 #/bin/bash
-# this script check file must be saved after reboot
+# this script check file must be saved before reboot
 
 cd "$(dirname "$0")"
 ip netns exec eth2_ns ip route add default via 192.168.99.254
 # Check ssh
-ip netns exec eth2_ns ./check_ssh.sh nopw > /dev/null ||
-{
-  ip netns exec eth2_ns fping -t 100 192.168.99.254 > /dev/null &&
-  ip netns exec eth3_ns fping -t 100 192.168.88.254 > /dev/null &&
-  ip netns exec eth4_ns fping -t 100 192.168.109.254 > /dev/null || 
-  {
-    ip netns exec eth2_ns ip route del default via 192.168.99.254
-    echo "ICMP is configured for INTRA"
-    exit 0
-  }
-}
+! ip netns exec eth2_ns ./check_ssh.sh nopw > /dev/null &&
+ip netns exec eth2_ns fping -t 100 192.168.99.254 > /dev/null &&
+ip netns exec eth3_ns fping -t 100 192.168.88.254 > /dev/null &&
+! ip netns exec eth4_ns fping -t 100 192.168.109.254 > /dev/null &&
+ip netns exec eth2_ns ip route del default via 192.168.99.254 &&
+echo "ICMP is configured for INTRA" &&
+exit 0
+
+
 ip netns exec eth2_ns ip route del default via 192.168.99.254
 echo "ICMP is not configured for INTRA!"
 exit 1
