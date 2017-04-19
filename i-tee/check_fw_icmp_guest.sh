c2f3d54f34f7ccfba8d1536add0b251348429576
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-19 15:43:09 +0300

    fw checks

---
+++ i-tee/check_fw_icmp_guest.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Script for checking VyOS - Getting Started objectives
+
+# Author - Katrin Loodus
+# Modified by Olari Pipenberg
+#
+# Date - 27.04.2016
+# Version - 0.0.1
+
+LC_ALL=C
+
+# START
+
+# Set variables
+
+START () {
+
+	# Enable logging
+	echo -e "\n$0 started on: $(date):" >> /var/log/labcheckslog.log
+	exec &> >(tee -a /var/log/labcheckslog.log)
+
+	# Objective uname in VirtualTA
+        Uname="vfwlocalicmpguest"
+
+	# If $CheckFile exists, then exit the script
+	CheckFile="/tmp/$Uname"
+
+	if [ -f $CheckFile ]; then echo "$0 has already ran successfully" && exit 0; fi
+
+    	# Exit if there are undeclared variables
+    	set -o nounset     
+
+	# Get working directory
+	DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+	# Time to sleep between running the check again
+	Sleep=1
+}
+
+VSERV () {
+	while true
+	do
+	bash /root/e-lab-check-scripts/check_objective/check_fw_icmp_guest.sh
+
+	if [ $? -eq 0 ]; then
+		# Run objectiveschecks.py and update VirtualTa with correct value
+		echo -e "\nStep Complete! Date: `date`\n" && touch $CheckFile
+        	$DIR/objectivechecks.py $Uname True || echo -e "\nFailed to run $DIR/objectiveschecks.py! Date: `date`" >&2 && exit 1
+        	exit 0			
+	fi
+	sleep $Sleep
+	done
+}
+
+START
+VSERV
+
+exit 0
+
