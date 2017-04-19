015d2e06b0965b8a381e5ae50e9d5863c74fc050
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-19 17:36:41 +0300

    critical error

--- check_objective/check_fw_ssh.sh
+++ check_objective/check_fw_ssh.sh
@@ -6,13 +6,13 @@ ip netns exec eth3_ns ip route add default via 192.168.88.254
 ip netns exec eth4_ns ip route add default via 192.168.109.254
 
 
-bash ./check_ssh.sh nopw > /dev/null &&
 # Check Workstation subnet
 ! ip netns exec eth2_ns bash ./check_ssh.sh nopw > /dev/null &&
 # Check Server subnet
 ! ip netns exec eth3_ns bash ./check_ssh.sh nopw > /dev/null &&
 # Check Guest subnet
 ! ip netns exec eth4_ns bash ./check_ssh.sh nopw > /dev/null && 
+bash ./check_ssh.sh nopw > /dev/null &&
 true
 
 if [ $? -eq 0  ]; then
