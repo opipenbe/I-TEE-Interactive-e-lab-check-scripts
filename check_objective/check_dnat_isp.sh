6f42cd0326570426b230d49a0f65247cd29b57c4
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-19 11:46:16 +0300

    fix

--- check_objective/check_dnat_isp.sh
+++ check_objective/check_dnat_isp.sh
@@ -2,14 +2,14 @@
 PORT="80"
 ADDRESS="30.0.127.3"
 
-nc -z $ADDRESS $PORT
+nc -z -w 1 $ADDRESS $PORT
 if [ $? -ne 0  ]; then
   echo "PAT for address: $ADDRESS and port: $PORT is not configured"
   exit 1
 fi
 
 # Check if dnat rule is too wide (ssh should not be open)
-nc -z $ADDRESS 22
+nc -z -w 1 $ADDRESS 22
 if [ $? -ne 0  ]; then
   echo "PAT for address: $ADDRESS and port: $PORT is configured"
   exit 0
--- check_objective/check_dnat_workstation.sh
+++ check_objective/check_dnat_workstation.sh
@@ -6,7 +6,7 @@ GW="192.168.99.254"
 
 ip netns exec $NSPACE ip route add default via $GW
 
-ip netns exec $NSPACE nc -z $ADDRESS $PORT
+ip netns exec $NSPACE nc -z -w 1 $ADDRESS $PORT
 if [ $? -ne 0  ]; then
   ip netns exec $NSPACE ip route del default via $GW
   echo "PAT for address: $ADDRESS and port: $PORT is not configured"
@@ -14,7 +14,7 @@ if [ $? -ne 0  ]; then
 fi
 
 # Check if dnat rule is too wide (ssh should not be open)
-ip netns exec $NSPACE nc -z $ADDRESS 22
+ip netns exec $NSPACE nc -z -w 1 $ADDRESS 22
 if [ $? -ne 0  ]; then
   ip netns exec $NSPACE ip route del default via $GW
   echo "PAT for address: $ADDRESS and port: $PORT is configured"
