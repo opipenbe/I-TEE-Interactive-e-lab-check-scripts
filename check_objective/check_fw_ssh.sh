e0c1a72165498e8c2885ddba78b6b6bf2a8279c1
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-19 09:48:03 +0300

    fw ssh

---
+++ check_objective/check_fw_ssh.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+# adding default gw
+ip netns exec eth2_ns ip route add default via 192.168.99.254
+ip netns exec eth3_ns ip route add default via 192.168.88.254
+ip netns exec eth4_ns ip route add default via 192.168.109.254
+
+
+bash ./check_ssh.sh > /dev/null &&
+# Check Workstation subnet
+ip netns exec eth2_ns bash ./check_ssh.sh nopw > /dev/null ||
+# Check Server subnet
+ip netns exec eth3_ns bash ./check_ssh.sh nopw > /dev/null ||
+# Check Guest subnet
+ip netns exec eth4_ns bash ./check_ssh.sh nopw > /dev/nul ||
+false
+
+if [ $? -eq 0  ]; then
+  ip netns exec eth2_ns ip route del default via 192.168.99.254
+  ip netns exec eth3_ns ip route del default via 192.168.88.254
+  ip netns exec eth4_ns ip route del default via 192.168.109.254
+  echo "SSH firewall is not configured"
+  exit 1
+fi
+
+ip netns exec eth2_ns ip route del default via 192.168.99.254
+ip netns exec eth3_ns ip route del default via 192.168.88.254
+ip netns exec eth4_ns ip route del default via 192.168.109.254
+echo "SSH firewall is configured"
+exit 0
--- check_objective/check_ssh.sh
+++ check_objective/check_ssh.sh
@@ -5,15 +5,17 @@ USERNAME="zeus"
 IP="30.0.127.2"
 PORT="28960"
 
-# Checks password auth
-ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=none -p $PORT $USERNAME@$IP 2>&1 >/dev/null | grep password > /dev/null
-if [ $? -eq 0  ]; then
-  echo "ssh password-auth is enabled (error)"
-  exit 1
+# If argument is set then we skip ssh password auth check
+if [ -z "$1"  ]; then
+  # Checks password auth
+  ssh -o ConnectTimeout=1 -o StrictHostKeyChecking=no -o PreferredAuthentications=none -p $PORT $USERNAME@$IP 2>&1 >/dev/null | grep password > /dev/null
+  if [ $? -eq 0  ]; then
+    echo "ssh password-auth is enabled (error)"
+    exit 1
+  fi
 fi
-
 # Checks public-key auth
-ssh -o StrictHostKeyChecking=no -q -i $KEY_PATH -p $PORT $USERNAME@$IP 'exit'
+ssh -o ConnectTimeout=1 -o StrictHostKeyChecking=no -q -i $KEY_PATH -p $PORT $USERNAME@$IP 'exit'
 if [ $? -eq 0  ]; then
   echo "ssh-public-key-auth is configured"
   exit 0
