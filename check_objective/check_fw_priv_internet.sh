7d5bd73d4e03a53954459703255aac824e9fb96b
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-19 11:23:33 +0300

    check internet

---
+++ check_objective/check_fw_priv_internet.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+# this script check file must be saved after reboot
+
+ip netns exec eth2_ns ip route add default via 192.168.99.254
+ip netns exec eth3_ns ip route add default via 192.168.88.254
+ip netns exec eth4_ns ip route add default via 192.168.109.254
+
+ip netns exec eth2_ns fping -t 100 30.0.127.1 > /dev/null &&
+ip netns exec eth3_ns fping -t 100 30.0.127.1 > /dev/null &&
+ip netns exec eth4_ns fping -t 100 30.0.127.1 > /dev/null &&
+{
+ip netns exec eth2_ns ip route del default via 192.168.99.254
+ip netns exec eth3_ns ip route del default via 192.168.88.254
+ip netns exec eth4_ns ip route del default via 192.168.109.254
+echo "Private networks have internet access"
+exit 0
+}
+
+ip netns exec eth2_ns ip route del default via 192.168.99.254
+ip netns exec eth3_ns ip route del default via 192.168.88.254
+ip netns exec eth4_ns ip route del default via 192.168.109.254
+echo "Private networks do not have internet access"
+exit 1
