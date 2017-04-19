94d49c2c2935cede328a5468d50e726f45bd13bc
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-19 14:36:09 +0300

    fix

--- check_objective/check_fw_ssh.sh
+++ check_objective/check_fw_ssh.sh
@@ -8,23 +8,23 @@ ip netns exec eth4_ns ip route add default via 192.168.109.254
 
 bash ./check_ssh.sh nopw > /dev/null &&
 # Check Workstation subnet
-ip netns exec eth2_ns bash ./check_ssh.sh nopw > /dev/null ||
+! ip netns exec eth2_ns bash ./check_ssh.sh nopw > /dev/null &&
 # Check Server subnet
-ip netns exec eth3_ns bash ./check_ssh.sh nopw > /dev/null ||
+! ip netns exec eth3_ns bash ./check_ssh.sh nopw > /dev/null &&
 # Check Guest subnet
-ip netns exec eth4_ns bash ./check_ssh.sh nopw > /dev/null ||
+! ip netns exec eth4_ns bash ./check_ssh.sh nopw > /dev/null && 
 true
 
 if [ $? -eq 0  ]; then
   ip netns exec eth2_ns ip route del default via 192.168.99.254
   ip netns exec eth3_ns ip route del default via 192.168.88.254
   ip netns exec eth4_ns ip route del default via 192.168.109.254
-  echo "Firewall for SSH is not configured"
-  exit 1
+  echo "Firewall for SSH is configured"
+  exit 0
 fi
 
 ip netns exec eth2_ns ip route del default via 192.168.99.254
 ip netns exec eth3_ns ip route del default via 192.168.88.254
 ip netns exec eth4_ns ip route del default via 192.168.109.254
-echo "Firewall for SSH is configured"
-exit 0
+echo "Firewall for SSH is not configured"
+exit 1
