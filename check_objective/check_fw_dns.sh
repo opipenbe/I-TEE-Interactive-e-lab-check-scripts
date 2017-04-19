35685e03a451bf26adb6289dc9545be9b41a47a4
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-19 10:57:18 +0300

    fw icmp

---
+++ check_objective/check_fw_dns.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bash check_dns_forwarding.sh eth2_ns 192.168.99.254 gw.lab.zz
+bash check_dns_forwarding.sh eth3_ns 192.168.88.254 gw.lab.zz
---
+++ check_objective/check_fw_icmp_guest.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+# this script check file must be saved after reboot
+ip netns exec eth4_ns ip route add default via 192.168.109.254
+ip netns exec eth4_ns ./check_ssh.sh nopw > /dev/null ||
+{
+  ip netns exec eth4_ns fping -t 100 192.168.109.254 > /dev/null &&
+  {
+    ip netns exec eth4_ns ip route del default via 192.168.109.254
+    echo "ICMP is configured for GUEST"
+    exit 0
+  }
+}
+ip netns exec eth4_ns ip route del default via 192.168.109.254
+echo "ICMP is not configured for GUEST!"
+exit 1
