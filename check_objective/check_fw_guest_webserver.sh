d1c571b4fc0d63ae00b1433dacd41c900b49583d
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-19 17:10:23 +0300

    fix

--- check_objective/check_fw_guest_webserver.sh
+++ check_objective/check_fw_guest_webserver.sh
@@ -2,6 +2,7 @@
 PORT="80"
 ADDRESS="192.168.88.200"
 
+# add default gw
 ip netns exec eth4_ns ip route add default via 192.168.109.254
 
 ip netns exec eth4_ns nc -z -w 1 $ADDRESS $PORT
@@ -11,14 +12,37 @@ if [ $? -ne 0  ]; then
   exit 1
 fi
 
+# Check rule-set destination address
+SRC_IP="192.168.109.1"
+DST_IP="192.168.88.1"
+# Start capturing HTTP traffic
+#ip netns exec eth3_ns tshark -i eth3  > /tmp/traffic < /dev/null 2>&1 &
+#-f "port 80 && host $DST_IP && host $SRC_IP" > /tmp/traffic < /dev/null 2>&1 &
+ip netns exec eth3_ns tshark -i eth3 > /tmp/traffic &
+
+# Send HTTP traffic
+ip netns exec eth4_ns nc -z -w 4 $DST_IP $PORT > /dev/null
+
+# Stop tshark
+pkill tshark
+wait $(jobs -rp) 2>/dev/null
+grep "$SRC_IP -> $DST_IP" /tmp/traffic > /dev/null
+if [ $? -eq 0  ]; then
+  ip netns exec eth4_ns ip route del default via 192.168.109.254
+  echo "Destination address is too wide"
+  exit 2
+fi
+
 # Check if firewall rule isnt too wide (ssh should not be open)
 ip netns exec eth4_ns nc -z -w 1 $ADDRESS 22
 if [ $? -ne 0  ]; then
+  # del def gw
   ip netns exec eth4_ns ip route del default via 192.168.109.254
   echo "Firewall: port: $PORT is configured for $ADDRESS"
   exit 0
 fi
 
+# del def gw
 ip netns exec eth4_ns ip route del default via 192.168.109.254
 echo "Firewall rule is too wide"
 exit 1
--- check_objective/check_fw_workstation_webserver.sh
+++ check_objective/check_fw_workstation_webserver.sh
@@ -11,6 +11,27 @@ if [ $? -ne 0  ]; then
   exit 1
 fi
 
+# Check rule-set destination address
+SRC_IP="192.168.99.1"
+DST_IP="192.168.88.1"
+# Start capturing HTTP traffic
+ip netns exec eth3_ns tshark -i eth3 > /tmp/traffic &
+
+# Send HTTP traffic
+ip netns exec eth2_ns nc -z -w 4 $DST_IP $PORT > /dev/null
+
+# Stop tshark
+pkill tshark
+wait $(jobs -rp) 2>/dev/null
+
+grep "$SRC_IP -> $DST_IP" /tmp/traffic > /dev/null
+if [ $? -eq 0  ]; then
+  ip netns exec eth4_ns ip route del default via 192.168.99.254
+  echo "Destination address is too wide"
+  exit 2
+fi
+
+
 # Check if firewall rule isnt too wide (ssh should not be open)
 ip netns exec eth2_ns nc -z -w 1 $ADDRESS 22
 if [ $? -ne 0  ]; then
