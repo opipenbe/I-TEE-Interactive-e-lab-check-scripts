a7b973e3bae6b40266b6ab21463fcb23035e9cd3
Olari Pipenberg <olari.pipenberg@gmail.com>
2017-04-20 08:31:37 +0300

    fix

--- check_objective/check_fw_icmp_guest.sh
+++ check_objective/check_fw_icmp_guest.sh
@@ -6,6 +6,7 @@ ip netns exec eth4_ns ip route add default via 192.168.109.254
 
 ! ip netns exec eth4_ns ./check_ssh.sh nopw > /dev/null &&
 ip netns exec eth4_ns fping -t 100 192.168.109.254 > /dev/null &&
+! ip netns exec eth4_ns ./check_ssh.sh nopw > /dev/null &&
 ip netns exec eth4_ns ip route del default via 192.168.109.254 &&
 echo "ICMP is configured for GUEST" &&
 exit 0
