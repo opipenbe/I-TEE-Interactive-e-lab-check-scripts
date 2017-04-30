0b520b018552dfaacfe041bb56d574b8e18c0157
opipenbe <olari.pipenberg@gmail.com>
2017-04-30 11:01:54 +0300

    Update set_interfaces.sh

--- netns_scripts/set_interfaces.sh
+++ netns_scripts/set_interfaces.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+
 # go to working directory
 cd "$(dirname "$0")"
+
 # this namespace is for workstation subnet
 ./create_namespace.sh eth2 eth2_ns 192.168.99.1/24
+
 # this namespace is for server subnet
 ./create_namespace.sh eth3 eth3_ns 192.168.88.1/24
+
 # this namespace is for guest subnet
 ./create_namespace.sh eth4 eth4_ns 192.168.109.1/24
+
 # this namespace is for ISP subnet
 ./create_namespace.sh eth5 eth5_ns 30.0.127.14/28
 
