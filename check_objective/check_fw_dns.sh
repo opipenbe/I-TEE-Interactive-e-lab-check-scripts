bf989ec11355e2d9d59f47522252b86dc1c01322
opipenbe <olari.pipenberg@gmail.com>
2017-05-01 21:52:27 +0300

    Update check_fw_dns.sh

--- check_objective/check_fw_dns.sh
+++ check_objective/check_fw_dns.sh
@@ -5,8 +5,6 @@
 cd "$(dirname "$0")"
 bash check_dns_forwarding.sh eth2_ns 192.168.99.254 gw.lab.zz > /dev/null &&
 bash check_dns_forwarding.sh eth3_ns 192.168.88.254 gw.lab.zz > /dev/null &&
-! bash check_dns_forwarding.sh eth4_ns 192.168.109.254 gw.lab.zz > /dev/null &&
-! bash check_dns_forwarding.sh eth5_ns 30.0.127.2 gw.lab.zz > /dev/null &&
 echo "DNS allowed (ok)" &&
 exit 0
 
